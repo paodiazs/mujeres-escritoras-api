3f2561be132461d5060c39e264c1e88e98482eec
Paola Díaz <45045278+paodiazs@users.noreply.github.com>
2026-03-13 17:10:20 -0600

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,16 @@
 import os
 import psycopg2
-from psycopg2.extras import RealDictCursor # Para que devuelva diccionarios
+from psycopg2.extras import RealDictCursor 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_connection():
-    # PostgreSQL usa 'dbname' en lugar de 'database'
+    
     return psycopg2.connect(
         host=os.getenv("BD_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         dbname=os.getenv("DB_NAME"),
-        port=os.getenv("DB_PORT", 5432) # Puerto por defecto de Postgres
+        port=os.getenv("DB_PORT") 
     )
