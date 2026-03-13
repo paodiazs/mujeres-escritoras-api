717a6e937a0298b1227b0669ea69f816980a7e4f
Paola Díaz <45045278+paodiazs@users.noreply.github.com>
2026-03-13 17:16:57 -0600

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,17 @@
 import os
 import psycopg2
-from psycopg2.extras import RealDictCursor 
-from dotenv import load_dotenv
-
-load_dotenv()
+from psycopg2.extras import RealDictCursor
 
 def get_connection():
+    url = os.getenv("DATABASE_URL")
     
-    return psycopg2.connect(
-        host=os.getenv("BD_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        dbname=os.getenv("DB_NAME"),
-        port=os.getenv("DB_PORT") 
-    )
+    if url:
+        return psycopg2.connect(url)
+    else:
+        return psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+            port=os.getenv("DB_PORT")
+        )
